348c47bf8344fc99a5d5643921ad9b1513392533
Jiozang theophane <jiozangtheophane@gmail.com>
2023-09-20 09:20:35 +0200

    merge

--- src/app/composants/Carousel/Carousel.jsx
+++ src/app/composants/Carousel/Carousel.jsx
@@ -1,58 +1,66 @@
 import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
 import './Carousel.css'
 
 function CarouselLogement({ logement }) {
     // Initialisez l'état local pour stocker le numéro de l'image actuelle
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const handlePrev = () => {
+    setCurrentIndex(currentIndex === 0 ? logement.pictures.length - 1 : currentIndex - 1);
+  };
+  
+  const handleNext = () => {
+    setCurrentIndex(currentIndex === logement.pictures.length - 1 ? 0 : currentIndex + 1);
+  };
   // Mettez à jour le numéro de l'image actuelle à chaque fois que l'utilisateur change d'image
   const handleSelect = (selectedIndex, e) => {
     setCurrentIndex(selectedIndex);
   };
-  return (
-    <Carousel
-    activeIndex={currentIndex}
-    onSelect={handleSelect}
-    prevIcon={<span className="carousel-control-prev-icon custom-icon" />}
-    nextIcon={<span aria-hidden="true" className="carousel-control-next-icon custom-icon" />}
-    >
-      {logement.pictures.map((item, index) => (
-        <Carousel.Item key={item} className='carousel-item'>
-          <img src={item} alt={item} className="d-block w-100 object-fit" />
-          {index === currentIndex && (
-            <Carousel.Caption>
-              <p className="mt-3">
-                {currentIndex + 1}/{logement.pictures.length}
-              </p>
-            </Carousel.Caption>
-          )}
-        </Carousel.Item>
-      ))}
-    </Carousel>
-    // <div id="demo" className="carousel slide dimension" data-bs-ride="carousel">
-    //   <div className="carousel-indicators">
-    //     {logement.pictures.map((item, index) =>
-    //         <button key={item} type="button" data-bs-target="#demo" data-bs-slide-to={index} className={index === 0 ? "active" : ""}></button>
-    //     )}
-    //   </div>
-
-    //   <div className="carousel-inner">
-    //   {logement.pictures.map((item, index) =>
-    //     <div className={`carousel-item ${index === 0 ? "active" : ""}`} key={item}>
-    //     <img src={item} alt={item} className="d-block w-100 object-fit" />
-    //   </div>
-    //   )}
-
-    //   </div>
-
-    //   <button className="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-    //     <span className="carousel-control-prev-icon"></span>
-    //   </button>
-    //   <button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-    //     <span className="carousel-control-next-icon"></span>
-    //   </button>
-    // </div>
+  return (  <div 
+    id="carouselExampleIndicators"
+     class="carousel slide"
+      data-bs-ride="carousel"
+      onSlide={(event) => {
+        const newIndex = parseInt(event.target.querySelector('.carousel-item.active').getAttribute('data-bs-slide-to'));
+        handleSelect(newIndex, event);
+      }}
+      >
+  <div class="carousel-indicators">
+    {logement.pictures.map((item, index) => (
+      <button
+        key={index}
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to={index}
+        className={index === currentIndex ? 'active' : ''}
+        aria-current={index === currentIndex ? 'true' : 'false'}
+        aria-label={`Slide ${index + 1}`}
+      ></button>
+    ))}
+  </div>
+  <div class="carousel-inner">
+    {logement.pictures.map((item, index) => (
+      <div key={item} className={`carousel-item ${index === currentIndex ? 'active' : ''}`}>
+        <img src={item} alt={item} class="d-block w-100" />
+        {index === currentIndex && (
+          <div class="carousel-caption d-none d-md-block">
+            <p className="mt-3">
+              {currentIndex + 1}/{logement.pictures.length}
+            </p>
+          </div>
+        )}
+      </div>
+    ))}
+  </div>
+  <button onClick={handlePrev} class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    {/* <span class="visually-hidden">Previous</span> */}
+  </button>
+  <button onClick={handleNext} class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    {/* <span class="visually-hidden">Next</span> */}
+  </button>
+</div>
   )
 }
 
--- src/app/composants/DetailLogement/DetailLogement.jsx
+++ src/app/composants/DetailLogement/DetailLogement.jsx
@@ -1,14 +1,16 @@
-import { Container } from 'react-bootstrap'
-import Accordion from 'react-bootstrap/Accordion'
 import './DetailLogement.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleUp, faAngleDown, faStar } from '@fortawesome/free-solid-svg-icons'
+import {
+  faAngleUp,
+  faAngleDown,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 
 function AccordionDetail({ logement }) {
   const [inputValue, handleClick] = useState(true)
   const [inputValue2, handleClick2] = useState(true)
-  const ratings = [1, 2, 3, 4, 5];
+  const ratings = [1, 2, 3, 4, 5]
   console.log(logement)
   return (
     <div className="mt-3">
@@ -16,81 +18,108 @@ function AccordionDetail({ logement }) {
         <div className="col-md-8" align="start">
           <div className="row">
             <div className="col-md-12 color-red line-height">
-                <h3>{logement.title}</h3>
-                <span>{logement.location}</span>
+              <h3>{logement.title}</h3>
+              <span>{logement.location}</span>
             </div>
             <div className="col-md-12 content-chips mt-3">
-                {logement.tags.map((tag) => 
-                <div className="chips" key={tag}>{tag}</div>)}
+              {logement.tags.map((tag) => (
+                <div className="chips" key={tag}>
+                  {tag}
+                </div>
+              ))}
             </div>
           </div>
         </div>
         <div className="col-md-4" align="end">
-        <div className="row">
+          <div className="row">
             <div className="col-md-12 color-red flex-right">
-                <h5>{logement.host.name}</h5>
-                <div className="rond"><img src={logement.host.picture} alt={logement.host.name} /></div>
+              <h5>{logement.host.name}</h5>
+              <div className="rond">
+                <img src={logement.host.picture} alt={logement.host.name} />
+              </div>
             </div>
             <div className="col-md-12 mt-4" align="end">
-            {ratings.map((tag) => tag > logement.rating ? (
-                <FontAwesomeIcon className="color-grid font-size" key={tag} icon={faStar} />
-            ) : <FontAwesomeIcon className="color-red font-size" key={tag} icon={faStar} />)}
+              {ratings.map((tag) =>
+                tag > logement.rating ? (
+                  <FontAwesomeIcon
+                    className="color-grid font-size"
+                    key={tag}
+                    icon={faStar}
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    className="color-red font-size"
+                    key={tag}
+                    icon={faStar}
+                  />
+                )
+              )}
             </div>
           </div>
         </div>
       </div>
       <div className="mt-5 row">
-        <Accordion className="col-md-6" defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header
-              onClick={() =>
-                inputValue === true ? handleClick(false) : handleClick(true)
-              }
-            >
-              <div className="bg-color-equipement">
-                <div>Description </div>
-                <div>
-                  {inputValue === true ? (
-                    <FontAwesomeIcon icon={faAngleUp} />
-                  ) : (
-                    <FontAwesomeIcon icon={faAngleDown} />
-                  )}
-                </div>
-              </div>
-            </Accordion.Header>
-            <Accordion.Body className="body-item">
-              {logement.description}
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        <div className="col-md-6">
+          <div
+            className="bg-color-equipement accordion-button d-flex justify-content-between align-items-center"
+            data-bs-toggle="collapse"
+            href="#descriptionCollapse"
+            role="button"
+            aria-expanded="false"
+            aria-controls="descriptionCollapse"
+            onClick={() => handleClick(!inputValue)}
+          >
+            <div>Description</div>
+            <div>
+              {inputValue ? (
+                <FontAwesomeIcon icon={faAngleUp} />
+              ) : (
+                <FontAwesomeIcon icon={faAngleDown} />
+              )}
+            </div>
+          </div>
+          {inputValue ? (
+            <div className="collapse show" id="descriptionCollapse">
+              <div className="body-item">{logement.description}</div>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
 
-        <Accordion className="col-md-6" defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header
-              onClick={() =>
-                inputValue2 === true ? handleClick2(false) : handleClick2(true)
-              }
-            >
-              <div className="bg-color-equipement">
-                <div>Equipements </div>
-                <div>
-                  {inputValue2 === true ? (
-                    <FontAwesomeIcon icon={faAngleUp} />
-                  ) : (
-                    <FontAwesomeIcon icon={faAngleDown} />
-                  )}
-                </div>
+        <div className="col-md-6">
+          <div
+            className="bg-color-equipement accordion-button d-flex justify-content-between align-items-center"
+            data-bs-toggle="collapse"
+            href="#equipmentCollapse"
+            role="button"
+            aria-expanded="false"
+            aria-controls="equipmentCollapse"
+            onClick={() => handleClick2(!inputValue2)}
+          >
+            <div>Equipements</div>
+            <div>
+              {inputValue2 ? (
+                <FontAwesomeIcon icon={faAngleUp} />
+              ) : (
+                <FontAwesomeIcon icon={faAngleDown} />
+              )}
+            </div>
+          </div>
+          {inputValue2 ? (
+            <div className="collapse show" id="equipmentCollapse">
+              <div className="body-item">
+                <ul className="no-bullet">
+                  {logement.equipments.map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
               </div>
-            </Accordion.Header>
-            <Accordion.Body className="body-item">
-              <ul className="no-bullet">
-                {logement.equipments.map((item) => (
-                  <li key={item}>{item}</li>
-                ))}
-              </ul>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
       </div>
     </div>
   )
